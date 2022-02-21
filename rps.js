367c94617194bbe5c3bfa77c2a40170c0e907c30
Harree <educationalmadz+GitHub@gmail.com>
2022-02-21 18:32:04 +0000

    much wow

--- rps.js
+++ rps.js
@@ -1,47 +1,4 @@
-/*
 
-for(i = 0; i < 5; i++) {
-   console.log(game());
-}
-
-
-function game() {
-function computerPlay() {
-
-    let computerChoices = ['Rock', 'Paper', 'Scissors'];
-    let randomChoice = Math.floor(Math.random() * computerChoices.length);
-    return computerChoices[randomChoice];
-
-}
-let computerSelection = computerPlay();
-// console.log(computerSelection)
-
-let playerPlay = function() {
-    let playerChoice = prompt("Rock, Paper or Scissors?")
-    if(playerChoice == null) {
-        return `cancelled`
-    } else
-    {return playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase(); }
-}
-let playerSelection = playerPlay();
-// console.log(playerSelection)
-
-    if(playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors") {
-        
-        return "Invalid Selection, please pick Rock, paper or scissors"
-
-    } else if((playerSelection == "Rock" && computerSelection == "Scissors") || (playerSelection == "Paper" && computerSelection == "Rock") || (playerSelection == "Scissors" && computerSelection == "Paper")) {
-        
-        return `You win! ${playerSelection} beats ${computerSelection}`
-
-    } else if(playerSelection == computerSelection) {
-        return `Draw!`
-    }
-    else {return `You lose! ${computerSelection} beats ${playerSelection}`}
-
-    
-}
-*/
 let playerScore = 0;
 let computerScore = 0;
 
@@ -58,7 +15,6 @@ function computerPlay() {
 
 }
 
-// let computerSelection = computerPlay(); //computer makes its choice.
 
 function singleRound(playerSelection, computerSelection){
 if((playerSelection == "Rock" && computerSelection == "Scissors") || (playerSelection == "Paper" && computerSelection == "Rock") || (playerSelection == "Scissors" && computerSelection == "Paper")) {
